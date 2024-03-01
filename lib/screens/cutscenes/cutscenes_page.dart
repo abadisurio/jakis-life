bc676045422c80d09c74e3b349f3f894531a3218
Abadi Suryo <abadisuryosetiyo@gmail.com>
2024-03-01 23:07:20 +0700

    Restore cutscene

--- lib/screens/cutscenes/cutscenes_page.dart
+++ lib/screens/cutscenes/cutscenes_page.dart
@@ -2,8 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:jakislife/bloc/player_bloc.dart';
-import 'package:jakislife/gen/assets.gen.dart';
-import 'package:rive/rive.dart';
+import 'package:jakislife/router/jakislife_route.dart';
 
 @RoutePage()
 class CutScenePage extends StatefulWidget {
@@ -20,55 +19,27 @@ class CutScenePage extends StatefulWidget {
 
 class _CutScenePageState extends State<CutScenePage> {
   String? currentGameName;
-
-  RiveGenImage? _cutSceneAnimation;
-
   @override
   void initState() {
     final bloc = context.read<PlayerBloc>()
       ..add(UpdateCurrentGameWin(isWin: widget.isWin));
-    // _riveController = OneShotAnimation('Berhasil');
-    // WidgetsBinding.instance.addPostFrameCallback((_) {
-    //   Future.delayed(const Duration(seconds: 3), () {
-    //     context.router.replace(const LifeCountRoute());
-    //   });
-    // });
-    // Assets.rive.telorGulung.rive();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Future.delayed(const Duration(seconds: 3), () {
+        context.router.replace(const LifeCountRoute());
+      });
+    });
     currentGameName = bloc.state.currentGame;
-    _cutSceneAnimation = _cutScene['TelorGulung'];
-    // setState(() {
-    //   _riveController.isActive = true;
-    // });
-    // _togglePlay();
     super.initState();
   }
-
-  // void _togglePlay() {
-  //   setState(() => _riveController.isActive = !_riveController.isActive);
-  // }
 
   @override
   Widget build(BuildContext context) {
     return Material(
       child: Center(
-        child: _cutSceneAnimation?.rive(
-          artboard: 'beli_telor_gulung',
-          // animations: ['Berhasil'],
-          // controllers: [_riveController],
-          onInit: (Artboard artboard) {
-            artboard.addController(SimpleAnimation('Timeline 1'));
-            setState(() {});
-          },
+        child: Text(
+          'isWin ${widget.isWin} previousPageName $currentGameName',
         ),
       ),
     );
   }
 }
-
-final _cutScene = {
-  'TelorGulung': Assets.rive.jaki,
-  // 'StableSidewalk': Assets.rive.telorGulung,
-  // 'CommuterRush': Assets.rive.telorGulung,
-  // 'LitterPicker': Assets.rive.telorGulung,
-  // 'CollectSocket': Assets.rive.telorGulung,
-};
